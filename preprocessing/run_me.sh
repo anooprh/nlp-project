d062991870e7d2746902cf75ed1907f70bf8305f
anooprh <anoop.hallur@gmail.com>
2014-10-29 00:07:49 -0400

    just need to run script to generate 100+MB of cleaned data

--- preprocessing/run_me.sh
+++ preprocessing/run_me.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+declare -a topics=("academia" "android" "anime" "apple" "arduino" "askubuntu" "astronomy" "aviation" "avp" "beer" "bicycles" "biology" "bitcoin" "blender" "boardgames" "bricks" "chemistry" "chess" "chinese" "christianity" "codegolf" "codereview" "cogsci" "cooking" "crypto" "cs" "cstheory" "dba" "diy" "drupal" "dsp" "earthscience" "ebooks" "electronics" "ell" "english" "expatriates" "expressionengine" "fitness" "freelancing" "french" "gamedev" "gaming" "gardening" "genealogy" "german" "gis" "graphicdesign" "ham")
 
 
-python prepare_input_helper.py -i ../data/stackexchange/academia.stackexchange.com.7z -o cleaned_data/academia_questions -w cleaned_data/academia_questions_with_id -t cleaned_data/academia_tags
+## now loop through the above array
+for i in "${topics[@]}"
+do
+   echo "Processing $i questions"
+   python prepare_input_helper.py -i ../data/stackexchange/"$i".stackexchange.com.7z  -o cleaned_data/"$i"_questions -w cleaned_data/"$i"_questions_with_id -t cleaned_data/"$i"_tags
+done
